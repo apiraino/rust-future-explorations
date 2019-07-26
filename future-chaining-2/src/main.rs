1d176ef09078af076430b898b38167e5591efb05
apiraino <apiraino@users.noreply.github.com>
2019-07-26 15:11:08 +0200

    does it make sense?

--- future-chaining-2/src/main.rs
+++ future-chaining-2/src/main.rs
@@ -16,13 +16,13 @@ impl MyClient {
         MyClient {}
     }
 
-    pub fn all_requests(&self) -> impl Future<Item = String, Error = ()> {
+    pub fn all_requests(&self) -> impl Future<Item = String, Error = ()> + '_ {
         let f1_res = self.request_one();
         let final_response = f1_res
-            .map(|resp1| {
-                eprintln!("req one resp: {}", resp1);
-                resp1
-            })
+            // .map(|resp1| {
+            //     eprintln!("req one resp: {}", resp1);
+            //     resp1
+            // })
             .and_then(move |resp1| {
                 self.request_two(resp1).and_then(|resp2| {
                     eprintln!("req two resp: {}", resp2);
@@ -37,12 +37,11 @@ impl MyClient {
         let url: Uri = "https://httpbin.org/post".parse().unwrap();
         let https = HttpsConnector::new(4).expect("TLS initialization failed");
         let client = Client::builder().build::<_, hyper::Body>(https);
-        let body = Body::from("key=val");
 
         let req = Request::builder()
             .method(Method::POST)
             .uri(url)
-            .body(body)
+            .body(Body::from("key=val"))
             .expect("Failed to build request body");
         let fut = client.request(req);
 
@@ -56,13 +55,11 @@ impl MyClient {
     }
 
     fn request_two(&self, param: String) -> impl Future<Item = String, Error = ()> {
-        eprintln!("Request two: {}", param);
+        eprintln!("Request two: {:?}", param);
         let url: Uri = "https://httpbin.org/get".parse().unwrap();
-
         let https = HttpsConnector::new(4).expect("TLS initialization failed");
         let client = Client::builder().build::<_, hyper::Body>(https);
 
-        eprintln!("contacting url: {}", url);
         let req = Request::builder()
             .uri(url)
             .body(Body::empty())
